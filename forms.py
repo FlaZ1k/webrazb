3a83ee3deac6d5c5cfc172dc0ddb271f8e895369
FlaZ1k <46661730+FlaZ1k@users.noreply.github.com>
2024-06-07 19:59:05 +0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import FileField, FloatField, SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf.file import FileRequired
+from wtforms.validators import DataRequired, ValidationError
+from flask_wtf.file import FileRequired, FileAllowed
+
+def image_file_check(form, field):
+    if not field.data.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+        raise ValidationError('Invalid file type. Please upload an image file.')
 
 class RotateImageForm(FlaskForm):
-    image = FileField('Upload Image', validators=[FileRequired()])
+    image = FileField('Upload Image', validators=[FileRequired(), image_file_check])
     angle = FloatField('Angle', validators=[DataRequired()])
     submit = SubmitField('Rotate Image')
+#
